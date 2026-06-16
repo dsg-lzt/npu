b940974a20549db5eb97fd5514e8bb7d0eb4e62d
dsg-lzt <dsg-lzt@users.noreply.github.com>
2026-06-16 17:51:30 +0800

    Fix: add temp to op_proto+op_host, kernel takes 5 params

--- op_proto/inc/furthest_point_sample_proto.h
+++ op_proto/inc/furthest_point_sample_proto.h
@@ -1,20 +1,17 @@
 #ifndef FURTHEST_POINT_SAMPLE_PROTO_H_
 #define FURTHEST_POINT_SAMPLE_PROTO_H_
+
 #include "graph/operator_reg.h"
+
 namespace ge {
+
 REG_OP(FurthestPointSample)
     .INPUT(points, ge::TensorType::ALL())
+    .INPUT(temp, ge::TensorType::ALL())
     .OUTPUT(sampled, ge::TensorType::ALL())
     .REQUIRED_ATTR(m, ge::AttrValue::INT)
     .OP_END_FACTORY_REG(FurthestPointSample);
-}
-#endif
 
-# Sync and build
-cp /home/lzt/ascend_project/fps/src/math/furthest_point_sample/op_kernel/furthest_point_sample.cpp \
-   /home/lzt/ascend_project/cann-ops/src/math/furthest_point_sample/op_kernel/
+}  // namespace ge
 
-source /home/lzt/miniconda3/bin/activate ascend_env
-export LD_LIBRARY_PATH="/home/lzt/Ascend/ascend-toolkit/8.3.RC1/lib64:/home/lzt/Ascend/ascend-toolkit/8.3.RC1/x86_64-linux/lib64:/home/lzt/Ascend/ascend-toolkit/8.3.RC1/x86_64-linux/devlib:${LD_LIBRARY_PATH}"
-cd /home/lzt/ascend_project/cann-ops && rm -rf build output && \
-bash build.sh -n furthest_point_sample -c ascend310p 2>&1 | tail -5
+#endif  // FURTHEST_POINT_SAMPLE_PROTO_H_
