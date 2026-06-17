efb29f8384126a9c2f5c7c3d275dc75f5c3e0654
dsg-lzt <dsg-lzt@github.com>
2026-06-17 14:13:46 +0800

    feat: add gather_custom operator for ascend310p with float16/float32 support

---
+++ src/index/gather_custom/op_host/gather_custom.cpp
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "gather_custom_tiling.h"
+#include "register/op_def_registry.h"
+#include "tiling/platform/platform_ascendc.h"
+#include "graph/utils/type_utils.h"
+
+namespace optiling {
+const uint64_t BLOCK_SIZE = 32;
+
+static ge::graphStatus TilingFunc(gert::TilingContext* context)
+{
+    GatherCustomTilingData tiling;
+
+    auto ascendcPlatform = platform_ascendc::PlatformAscendC(context->GetPlatformInfo());
+    uint64_t ubLength = 0;
+    ascendcPlatform.GetCoreMemSize(platform_ascendc::CoreMemType::UB, ubLength);
+    auto coreNum = ascendcPlatform.GetCoreNum();
+
+    uint64_t numIndices = context->GetInputShape(1)->GetStorageShape().GetShapeSize();
+    uint64_t inputTotal = context->GetInputShape(0)->GetStorageShape().GetShapeSize();
+    uint64_t inputDim0 = context->GetInputShape(0)->GetStorageShape().GetDim(0);
+    uint64_t sliceLength = (inputDim0 == 0) ? 0 : (inputTotal / inputDim0);
+
+    uint32_t dataTypeLength = 0;
+    ge::TypeUtils::GetDataTypeLength(context->GetInputDesc(0)->GetDataType(), dataTypeLength);
+    if (dataTypeLength == 0) {
+        return ge::GRAPH_FAILED;
+    }
+
+    uint64_t ubSliceLen = (ubLength / 4) / dataTypeLength;
+    ubSliceLen = (ubSliceLen / (BLOCK_SIZE / dataTypeLength)) * (BLOCK_SIZE / dataTypeLength);
+    if (ubSliceLen == 0) {
+        ubSliceLen = BLOCK_SIZE / dataTypeLength;
+    }
+
+    uint64_t sliceLoopNum;
+    uint64_t sliceTailLen;
+    if (ubSliceLen >= sliceLength) {
+        ubSliceLen = sliceLength;
+        sliceLoopNum = 1;
+        sliceTailLen = sliceLength;
+    } else {
+        sliceLoopNum = sliceLength / ubSliceLen;
+        sliceTailLen = (sliceLength % ubSliceLen == 0) ? ubSliceLen : (sliceLength % ubSliceLen);
+    }
+
+    uint64_t indicesPerCore = numIndices / coreNum;
+    uint64_t tailIndices = numIndices % coreNum;
+    uint64_t smallCoreIndicesNum = indicesPerCore;
+    uint64_t bigCoreIndicesNum = indicesPerCore + (tailIndices > 0 ? 1 : 0);
+
+    tiling.set_numIndices(numIndices);
+    tiling.set_sliceLength(sliceLength);
+    tiling.set_sliceLoopNum(sliceLoopNum);
+    tiling.set_ubSliceLen(ubSliceLen);
+    tiling.set_sliceTailLen(sliceTailLen);
+    tiling.set_smallCoreIndicesNum(smallCoreIndicesNum);
+    tiling.set_bigCoreIndicesNum(bigCoreIndicesNum);
+    tiling.set_tailBlockNum(tailIndices);
+
+    context->SetBlockDim(coreNum);
+    tiling.SaveToBuffer(context->GetRawTilingData()->GetData(), context->GetRawTilingData()->GetCapacity());
+    context->GetRawTilingData()->SetDataSize(tiling.GetDataSize());
+
+    size_t* currentWorkspace = context->GetWorkspaceSizes(1);
+    currentWorkspace[0] = 0;
+    return ge::GRAPH_SUCCESS;
+}
+}
+
+namespace ge {
+static ge::graphStatus InferShape(gert::InferShapeContext* context)
+{
+    const auto* xShape = context->GetInputShape(0);
+    const auto* idxShape = context->GetInputShape(1);
+    auto* yShape = context->GetOutputShape(0);
+
+    int64_t xDimNum = xShape->GetDimNum();
+    int64_t idxDimNum = idxShape->GetDimNum();
+    int64_t yDimNum = xDimNum - 1 + idxDimNum;
+
+    yShape->SetDimNum(yDimNum);
+    for (int64_t i = 0; i < idxDimNum; ++i) {
+        yShape->SetDim(i, idxShape->GetDim(i));
+    }
+    for (int64_t i = 0; i < xDimNum - 1; ++i) {
+        yShape->SetDim(idxDimNum + i, xShape->GetDim(i + 1));
+    }
+    return GRAPH_SUCCESS;
+}
+
+static graphStatus InferDataType(gert::InferDataTypeContext* context)
+{
+    const auto inputDataType = context->GetInputDataType(0);
+    context->SetOutputDataType(0, inputDataType);
+    return ge::GRAPH_SUCCESS;
+}
+}
+
+namespace ops {
+class GatherCustom : public OpDef {
+public:
+    explicit GatherCustom(const char* name) : OpDef(name)
+    {
+        this->Input("x")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Input("indices")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_INT32, ge::DT_INT32})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+        this->Output("y")
+            .ParamType(REQUIRED)
+            .DataType({ge::DT_FLOAT16, ge::DT_FLOAT})
+            .Format({ge::FORMAT_ND, ge::FORMAT_ND})
+            .UnknownShapeFormat({ge::FORMAT_ND, ge::FORMAT_ND});
+
+        this->SetInferShape(ge::InferShape).SetInferDataType(ge::InferDataType);
+        this->AICore()
+            .SetTiling(optiling::TilingFunc)
+            .AddConfig("ascend310p");
+    }
+};
+
+OP_ADD(GatherCustom);
+}
---
+++ src/index/gather_custom/op_kernel/gather_custom.cpp
@@ -0,0 +1,129 @@
+/**
+ * Copyright (c) Huawei Technologies Co., Ltd. 2025. All rights reserved.
+ * This file is a part of the CANN Open Software.
+ * Licensed under CANN Open Software License Agreement Version 1.0 (the "License").
+ * Please refer to the License for details. You may not use this file except in compliance with the License.
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
+ * See LICENSE in the root of the software repository for the full text of the License.
+ */
+
+#include "kernel_operator.h"
+using namespace AscendC;
+constexpr int32_t BUFFER_NUM = 2;
+
+template<typename T>
+class KernelGatherCustom {
+public:
+    __aicore__ inline KernelGatherCustom() {}
+    __aicore__ inline void Init(
+        GM_ADDR x, GM_ADDR indices, GM_ADDR y,
+        uint64_t numIndices, uint64_t sliceLength,
+        uint64_t sliceLoopNum, uint64_t ubSliceLen, uint64_t sliceTailLen,
+        uint64_t smallCoreIndicesNum, uint64_t bigCoreIndicesNum, uint64_t tailBlockNum,
+        TPipe* pipeIn)
+    {
+        pipe = pipeIn;
+        this->numIndices = numIndices;
+        this->sliceLength = sliceLength;
+        this->sliceLoopNum = sliceLoopNum;
+        this->ubSliceLen = ubSliceLen;
+        this->sliceTailLen = sliceTailLen;
+
+        uint64_t coreIdx = AscendC::GetBlockIdx();
+        if (coreIdx < tailBlockNum) {
+            this->myIndicesNum = bigCoreIndicesNum;
+        } else {
+            this->myIndicesNum = smallCoreIndicesNum;
+        }
+
+        this->indicesStart = 0;
+        for (uint64_t i = 0; i < coreIdx; ++i) {
+            this->indicesStart += (i < tailBlockNum) ? bigCoreIndicesNum : smallCoreIndicesNum;
+        }
+        this->indicesEnd = this->indicesStart + this->myIndicesNum;
+
+        xGm.SetGlobalBuffer((__gm__ T*)x);
+        yGm.SetGlobalBuffer((__gm__ T*)y);
+        this->indicesGm = (__gm__ int32_t*)indices;
+
+        pipe->InitBuffer(inQueue, BUFFER_NUM, this->ubSliceLen * sizeof(T));
+        pipe->InitBuffer(outQueue, BUFFER_NUM, this->ubSliceLen * sizeof(T));
+    }
+
+    __aicore__ inline void Process()
+    {
+        for (uint64_t idx = this->indicesStart; idx < this->indicesEnd; ++idx) {
+            int32_t gatherIdx = this->indicesGm[idx];
+            uint64_t srcOffset = gatherIdx * this->sliceLength;
+            uint64_t dstOffset = idx * this->sliceLength;
+
+            for (uint64_t tile = 0; tile < this->sliceLoopNum; ++tile) {
+                CopyIn(srcOffset + tile * this->ubSliceLen, this->ubSliceLen);
+                CopyOut(dstOffset + tile * this->ubSliceLen, this->ubSliceLen);
+            }
+            if (this->sliceLoopNum * this->ubSliceLen < this->sliceLength) {
+                uint64_t tailLen = this->sliceTailLen;
+                if (this->sliceLoopNum == 0) {
+                    tailLen = this->sliceLength;
+                }
+                CopyIn(srcOffset + this->sliceLoopNum * this->ubSliceLen, tailLen);
+                CopyOut(dstOffset + this->sliceLoopNum * this->ubSliceLen, tailLen);
+            }
+        }
+    }
+
+private:
+    __aicore__ inline void CopyIn(uint64_t offset, uint64_t len)
+    {
+        LocalTensor<T> local = inQueue.AllocTensor<T>();
+        DataCopy(local, xGm[offset], len);
+        inQueue.EnQue(local);
+    }
+
+    __aicore__ inline void CopyOut(uint64_t offset, uint64_t len)
+    {
+        LocalTensor<T> local = outQueue.DeQue<T>();
+        DataCopy(yGm[offset], local, len);
+        outQueue.FreeTensor(local);
+    }
+
+private:
+    GlobalTensor<T> xGm;
+    GlobalTensor<T> yGm;
+    __gm__ int32_t* indicesGm;
+    TPipe* pipe;
+    TQue<QuePosition::VECIN, BUFFER_NUM> inQueue;
+    TQue<QuePosition::VECOUT, BUFFER_NUM> outQueue;
+
+    uint64_t numIndices;
+    uint64_t sliceLength;
+    uint64_t sliceLoopNum;
+    uint64_t ubSliceLen;
+    uint64_t sliceTailLen;
+    uint64_t myIndicesNum;
+    uint64_t indicesStart;
+    uint64_t indicesEnd;
+};
+
+extern "C" __global__ __aicore__ void gather_custom(
+    GM_ADDR x, GM_ADDR indices, GM_ADDR y, GM_ADDR workspace, GM_ADDR tiling)
+{
+    GET_TILING_DATA(tiling_data, tiling);
+    TPipe pipe;
+    KernelGatherCustom<DTYPE_X> op;
+    op.Init(x, indices, y,
+            tiling_data.numIndices, tiling_data.sliceLength,
+            tiling_data.sliceLoopNum, tiling_data.ubSliceLen, tiling_data.sliceTailLen,
+            tiling_data.smallCoreIndicesNum, tiling_data.bigCoreIndicesNum, tiling_data.tailBlockNum,
+            &pipe);
+    op.Process();
+}
+
+#ifndef ASCENDC_CPU_DEBUG
+void gather_custom_do(uint32_t blockDim, void* l2ctrl, void* stream, uint8_t* x, uint8_t* indices, uint8_t* y,
+    uint8_t* workspace, uint8_t* tiling)
+{
+    gather_custom<<<blockDim, l2ctrl, stream>>>(x, indices, y, workspace, tiling);
+}
+#endif
